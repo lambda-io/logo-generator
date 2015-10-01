e1e79cdf1c6c35437a0ebf1b6528b4ad9e268162
Ian <ianramosc@gmail.com>
2015-10-01 20:57:21 -0300

    Initial city name change

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-main.js
+var cityInput = document.getElementById('cityInput');
+var pattern = document.getElementById('lambda_pattern');
+
+cityInput.onchange = function() {
+	var logoSVG = pattern.getSVGDocument();
+	var cityName = logoSVG.getElementById('cityName');
+	cityName.textContent = this.value;
+}
+
+cityInput.onkeyup = cityInput.onkeydown = cityInput.onchange;
